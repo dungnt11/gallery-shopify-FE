70f3a47e04904cf6114788d5616e2449cc9518f9
Dung Nguyen <zzhongmiuzz@hotmail.com>
2021-10-23 22:31:23 +0700

    Feat: layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,107 @@
 import { getJsonByShop } from './helper/get-json';
+import { LOADING_SVG } from './constant';
 
-class Main {
+class Gallery {
   constructor() {
-    this.init();    
+    this.loadingDOM = LOADING_SVG;
   }
   
   async init() {
-    try {
-      const galleryDB = await getJsonByShop();
-      console.log(galleryDB);
-      let gallery     = galleryDB.gallery,
-          colGap      = gallery.columnGap,
-          rowGap      = gallery.rowGap,
-          title       = gallery.title,
-          description = gallery.description,
-          effect      = gallery.effectSelect
-      let _html = `<div class="container">
-                    <div class="dtt-heading"><h2>${title}</h2><p>${description}</p></div>
-                    <div class="dtt-grid" 
-                       style="--columnGap_xl: ${colGap.xl}px;--columnGap_lg: ${colGap.lg}px;--columnGap_md: ${colGap.md}px;--columnGap_sm: ${colGap.sm}px;--columnGap_xs: ${colGap.xs}px;
-                              --rowGap_xl: ${rowGap.xl}px;--rowGap_lg: ${rowGap.lg}px;--rowGap_md: ${rowGap.md}px;--rowGap_sm: ${rowGap.sm}px;--rowGap_xs: ${rowGap.xs}px;">`;
+    const galleriesDOM = document.querySelectorAll('e-gallery-widget');
 
-      galleryDB.images.map(function (v, i) {
-        console.log(v);
-        _html += `
-          <div class="dtt-col">
-            <div class="hovereffect${effect} e-image-item" style="background: ${v.effect.color};">
-              <img class="img-responsive" src="${v.src}" alt="">
-              <div class="overlay">
-                <h2>${v.effect.title}</h2>
-                <a class="info" href="${v.effect.link}">${v.effect.description}</a>
-              </div>
-            </div>
-          </div>
-        `
+    if (galleriesDOM.length) {
+      Array.from(galleriesDOM).forEach((galleryDOMItem) => {
+        this.genGallery(galleryDOMItem);
       });
-      _html += '</div></div>';
-      document.querySelectorAll(".hello")[0].innerHTML = _html;
+    }
+  }
 
+  async genGallery(galleryDOMArg) {
+    try {
+      const galleryDOM = galleryDOMArg.getAttribute('data-id');
+      galleryDOMArg.innerHTML = this.loadingDOM;
+      const galleryDB = await getJsonByShop(galleryDOM);
+      const totalBlockY = this.getLastBlock(galleryDB.images);
+
+      galleryDOMArg.style.cssText = this.buildCSSGallery(totalBlockY, galleryDB.gallery);
+      galleryDOMArg.innerHTML = this.buildImageGallery(galleryDB.images);
     } catch (error) {
+      // Nếu có lỗi sẽ thông báo ở đây
       console.log(error);
     }
   }
+
+  getLastBlock(images) {
+    let maxY = 0, ind = [];
+    images.forEach(({ layout }) => {
+      if ((layout.xl.y + layout.xl.h) >= maxY) {
+        maxY = layout.xl.y + layout.xl.h;
+      }
+    });
+
+    return maxY;
+  }
+
+  // Hàm này sẽ build css cho gallery
+  buildCSSGallery(totalBlockY, gallerySettings) {
+    const {
+      // title, description, positionTitle,
+      settings,
+      // Gap grid
+      rowGap, columnGap,
+    } = gallerySettings;
+
+    // !Đoạn này sau cần làm responsive
+    return `grid-gap: ${rowGap.xl}px ${columnGap.xl}px;
+    grid-template-rows: repeat(${totalBlockY}, ${settings.rowHeight}px)`;
+  }
+
+  // Item trong gallery
+  buildImageGallery(images) {
+    let imagesDOM = '';
+
+    images.forEach((image) => {
+      imagesDOM += `
+        <div class="e-gallery__item" ${this.genCSSImageLayout(image.layout)}>
+          ${this.genImageByEffect(image.effect)}
+          <img class="e-gallery__image" alt="${image.alt}" src="${image.src}" />
+        </div>
+      `;
+    });
+
+    return imagesDOM;
+  }
+
+  // Build css layout cho item
+  genCSSImageLayout(layout) {
+    let cssImageLayout = '';
+
+    Object.keys(layout).forEach((display) => {
+      const layoutObj = layout[display];
+      // display là tên màn hình
+      switch (display) {
+        case 'xl': {
+          cssImageLayout += `grid-column: ${layoutObj.x + 1}/ span ${layoutObj.w};
+          grid-row: ${layoutObj.y + 1}/ span ${layoutObj.h}`;
+          break;
+        }
+      
+        default:
+          break;
+      }
+    });
+
+    return cssImageLayout ? `style="${cssImageLayout}"` : '';
+  }
+
+  // Build image effect cho từng ảnh
+  genImageByEffect(effect) {
+    //
+
+    return '';
+  }
 }
 
-new Main();
+const gallery = new Gallery();
+// Initial
+gallery.init();
