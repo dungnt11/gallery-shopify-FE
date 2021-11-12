28d80306cce939b45f9004923b6ef688591542f4
Dung Nguyen <zzhongmiuzz@hotmail.com>
2021-11-12 20:38:12 +0700

    Fix: URL test

--- src/js/helper/get-json.js
+++ src/js/helper/get-json.js
@@ -1,13 +1,15 @@
 import { getShop } from './get-shop';
 
-/**
- * Lấy tên shop từ domain
- */
 async function getJsonByShop(handleGallery) {
   const hostname = window.location.hostname;
   if (!hostname && process.env.NODE_ENV !== 'development') {return;}
   const shopName = getShop(hostname);
-  const urlJSON = process.env.NODE_ENV === 'development' ? `${process.env.URL_JSON_GALLERY}?time=${Date.now()}` : `${process.env.URL_BASE}/${shopName}/${handleGallery}.json`;
+  let urlJSON = `${process.env.URL_GALLERY_APP}/${shopName}/${handleGallery}.json`;
+  if (!window.location.search.includes('?preview=true&gallery=') && window.location.search.includes('.json')) {
+    alert(`URL preview phải có dạng ${process.env.URL_GALLERY_APP}/?preview=true&gallery=doremon-shop/handle_gallery.json`);
+  } else {
+    urlJSON = `${process.env.URL_GALLERY_APP}/${window.location.search.replace('?preview=true&gallery=', '')}?time=${Date.now()}`;
+  }
   const resGallery = await fetch(urlJSON, {
     'headers': {
       'accept': '*/*',
