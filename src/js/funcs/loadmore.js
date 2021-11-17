eef321ff2d5423954667fae0133dc4667cd77903
tuandt2 <doanthanhtuan110793@gmail.com>
2021-11-18 00:02:17 +0700

    add: css loadmore

--- src/js/funcs/loadmore.js
+++ src/js/funcs/loadmore.js
@@ -51,7 +51,21 @@ function loadmoreFn(galleryDOM, galleryDB, blockSorted, displayActive) {
     const galleryID = galleryDOM.getAttribute('data-id');
     // Fix resize reinit quá nhiều
     if (document.querySelector(`button[id="${galleryID}"]`)) return;
-    const btn = `<button id="${galleryID}">Loadmore</button>`;
+    const btn = `<button class="e-btn__loadmore" data="left-loading" id="${galleryID}">
+    <svg class="animated fadeInRight" version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       width="24px" height="24px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+      <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+        <animateTransform attributeType="xml"
+                          attributeName="transform"
+                          type="rotate"
+                          from="0 25 25"
+                          to="360 25 25"
+                          dur="0.6s"
+                          repeatCount="indefinite"/>
+      </path>
+    </svg>
+    <span class="text">Loadmore</span>
+    </button>`;
     galleryDOM.insertAdjacentHTML('afterend', btn);
     const buttonLoadmoreDOM = document.querySelector(`button[id="${galleryID}"]`);
     if (!buttonLoadmoreDOM) return;
