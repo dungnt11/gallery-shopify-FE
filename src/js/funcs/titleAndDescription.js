cde601b026478581a2f16542c270736f0998e970
Dung Nguyen <zzhongmiuzz@hotmail.com>
2021-11-18 19:13:15 +0700

    Fix: init title quá nhiều

--- src/js/funcs/titleAndDescription.js
+++ src/js/funcs/titleAndDescription.js
@@ -1,6 +1,9 @@
 function titleAndDescriptionFnc(blockDOM, gallerySettings) {
+  const dataIDGallery = blockDOM.getAttribute('data-id');
+  if (!dataIDGallery) return;
+  if (document.querySelector(`.e-gallery__heading[data-id="${dataIDGallery}"]`)) return;
   const { title, description, positionTitle } = gallerySettings;
-  const DOMTitleAndDes = `<div class="e-gallery__heading ${positionTitle}">
+  const DOMTitleAndDes = `<div class="e-gallery__heading ${positionTitle}" data-id="${dataIDGallery}">
       <div class="e-gallery__heading--title">${title}</div>
       <div class="e-gallery__heading--description">${description}</div>
     </div>`;
