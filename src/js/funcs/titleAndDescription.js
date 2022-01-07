16c616b2129888052da3a7e85448872d079dbaac
nguyen dung <zzhongmiuzz@hotmail.com>
2022-01-07 15:09:52 +0000

    Fix: spacing title & description

--- src/js/funcs/titleAndDescription.js
+++ src/js/funcs/titleAndDescription.js
@@ -3,7 +3,7 @@ function titleAndDescriptionFnc(blockDOM, gallerySettings) {
   if (!dataIDGallery) return;
   if (document.querySelector(`.e-gallery__heading[data-id="${dataIDGallery}"]`)) return;
   const { title, description, positionTitle } = gallerySettings;
-  const DOMTitleAndDes = `<div class="e-gallery__heading ${positionTitle}" data-id="${dataIDGallery}">
+  const DOMTitleAndDes = `<div class="e-gallery__heading e-gallery-spacing-bottom ${positionTitle}" data-id="${dataIDGallery}">
       ${title}
       ${description}
     </div>`;
